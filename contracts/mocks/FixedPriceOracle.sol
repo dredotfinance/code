bd07e11bb44aa6098e2c87886ff117701e53248c
Dre Cronye <andre@dre.finance>
2025-06-01 14:33:40 +0000

    Updated contracts

---
+++ contracts/mocks/FixedPriceOracle.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "../interfaces/IAggregatorV3.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FixedPriceOracle is AggregatorV3Interface, Ownable {
+    uint8 private _decimals;
+    int256 private _price;
+
+    constructor(uint8 decimals_, int256 price_) Ownable(msg.sender) {
+        _decimals = decimals_;
+        _price = price_;
+    }
+
+    function decimals() external view override returns (uint8) {
+        return _decimals;
+    }
+
+    function description() external pure override returns (string memory) {
+        return "Mock AggregatorV3";
+    }
+
+    function version() external pure override returns (uint256) {
+        return 1;
+    }
+
+    function getRoundData(
+        uint80 _roundId
+    )
+        external
+        view
+        override
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
+    {
+        return (_roundId, _price, block.timestamp, block.timestamp, _roundId);
+    }
+
+    function latestRoundData()
+        external
+        view
+        override
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
+    {
+        return (1, _price, block.timestamp, block.timestamp, 1);
+    }
+
+    function latestAnswer() external view override returns (int256) {
+        return _price;
+    }
+
+    function setPrice(int256 price_) external onlyOwner {
+        _price = price_;
+    }
+}
