11e6698b8377d03e40617407f860659d37c1c6c2
Dre Cronye <dre@rezerve.money>
2025-06-12 09:00:09 +0000

    Updated contracts

--- contracts/AppAuthority.sol
+++ contracts/AppAuthority.sol
@@ -32,111 +32,140 @@ contract AppAuthority is IAppAuthority, AccessControlEnumerable {
         _setRoleAdmin(RESERVE_MANAGER_ROLE, GOVERNOR_ROLE);
     }
 
+    /* ========== MODIFIERS ========== */
+
     modifier onlyGovernor() {
         require(hasRole(GOVERNOR_ROLE, msg.sender), "Only governor");
         _;
     }
 
+    /// @inheritdoc IAppAuthority
     function setOperationsTreasury(address _newOperationsTreasury) external onlyGovernor {
         address oldOperationsTreasury = operationsTreasury;
         operationsTreasury = _newOperationsTreasury;
         emit OperationsTreasuryUpdated(_newOperationsTreasury, oldOperationsTreasury);
     }
 
+    /// @inheritdoc IAppAuthority
     function setTreasury(address _newTreasury) external onlyGovernor {
         address oldTreasury = address(treasury);
         treasury = IAppTreasury(_newTreasury);
         emit TreasuryUpdated(_newTreasury, oldTreasury);
     }
 
+    /// @inheritdoc IAppAuthority
     function addGovernor(address _newGovernor) external onlyGovernor {
         _grantRole(GOVERNOR_ROLE, _newGovernor);
     }
 
+    /// @inheritdoc IAppAuthority
     function addGuardian(address _newGuardian) external onlyGovernor {
         _grantRole(GUARDIAN_ROLE, _newGuardian);
     }
 
+    /// @inheritdoc IAppAuthority
     function addPolicy(address _newPolicy) external onlyGovernor {
         _grantRole(POLICY_ROLE, _newPolicy);
     }
 
+    /// @inheritdoc IAppAuthority
     function addReserveManager(address _newReserveManager) external onlyGovernor {
         _grantRole(RESERVE_MANAGER_ROLE, _newReserveManager);
     }
 
+    /// @inheritdoc IAppAuthority
     function addReserveDepositor(address _newReserveDepositor) external onlyGovernor {
         _grantRole(RESERVE_DEPOSITOR_ROLE, _newReserveDepositor);
     }
 
+    /// @inheritdoc IAppAuthority
     function addBondManager(address _newBondManager) external onlyGovernor {
         _grantRole(BOND_MANAGER_ROLE, _newBondManager);
     }
 
+    /// @inheritdoc IAppAuthority
     function addExecutor(address _newExecutor) external onlyGovernor {
         _grantRole(EXECUTOR_ROLE, _newExecutor);
     }
 
+    /// @inheritdoc IAppAuthority
     function removeGovernor(address _oldGovernor) external onlyGovernor {
         _revokeRole(GOVERNOR_ROLE, _oldGovernor);
     }
 
+    /// @inheritdoc IAppAuthority
     function removeGuardian(address _oldGuardian) external onlyGovernor {
         _revokeRole(GUARDIAN_ROLE, _oldGuardian);
     }
 
+    /// @inheritdoc IAppAuthority
     function removePolicy(address _oldPolicy) external onlyGovernor {
         _revokeRole(POLICY_ROLE, _oldPolicy);
     }
 
+    /// @inheritdoc IAppAuthority
     function removeReserveManager(address _oldReserveManager) external onlyGovernor {
         _revokeRole(RESERVE_MANAGER_ROLE, _oldReserveManager);
     }
 
+    /// @inheritdoc IAppAuthority
     function removeReserveDepositor(address _oldReserveDepositor) external onlyGovernor {
         _revokeRole(RESERVE_DEPOSITOR_ROLE, _oldReserveDepositor);
     }
 
+    /// @inheritdoc IAppAuthority
     function removeExecutor(address _oldExecutor) external onlyGovernor {
         _revokeRole(EXECUTOR_ROLE, _oldExecutor);
     }
 
+    /// @inheritdoc IAppAuthority
     function removeBondManager(address _oldBondManager) external onlyGovernor {
         _revokeRole(BOND_MANAGER_ROLE, _oldBondManager);
     }
 
+    /* ========== VIEWS ========== */
+
+    /// @inheritdoc IAppAuthority
     function isGovernor(address account) external view override returns (bool) {
         return hasRole(GOVERNOR_ROLE, account);
     }
 
+    /// @inheritdoc IAppAuthority
     function isReserveDepositor(address account) external view override returns (bool) {
         return hasRole(RESERVE_DEPOSITOR_ROLE, account);
     }
 
+    /// @inheritdoc IAppAuthority
     function isReserveManager(address account) external view override returns (bool) {
         return hasRole(RESERVE_MANAGER_ROLE, account);
     }
 
+    /// @inheritdoc IAppAuthority
     function isGuardian(address account) external view override returns (bool) {
         return hasRole(GUARDIAN_ROLE, account);
     }
 
+    /// @inheritdoc IAppAuthority
     function isPolicy(address account) external view override returns (bool) {
         return hasRole(POLICY_ROLE, account);
     }
 
+    /// @inheritdoc IAppAuthority
     function isTreasury(address account) external view override returns (bool) {
         return account == address(treasury);
     }
 
+    /// @inheritdoc IAppAuthority
     function isExecutor(address account) external view override returns (bool) {
         return hasRole(EXECUTOR_ROLE, account);
     }
 
+    /// @inheritdoc IAppAuthority
     function isBondManager(address account) external view override returns (bool) {
         return hasRole(BOND_MANAGER_ROLE, account);
     }
 
+    /// @inheritdoc IAppAuthority
     function getAllCandidates(bytes32 role) public view returns (address[] memory candidates) {
         uint256 count = getRoleMemberCount(role);
         candidates = new address[](count);
@@ -145,30 +174,37 @@ contract AppAuthority is IAppAuthority, AccessControlEnumerable {
         }
     }
 
+    /// @inheritdoc IAppAuthority
     function getAllReserveDepositorCandidates() external view returns (address[] memory candidates) {
         return getAllCandidates(RESERVE_DEPOSITOR_ROLE);
     }
 
+    /// @inheritdoc IAppAuthority
     function getAllExecutorCandidates() external view returns (address[] memory candidates) {
         return getAllCandidates(EXECUTOR_ROLE);
     }
 
+    /// @inheritdoc IAppAuthority
     function getAllPolicyCandidates() external view returns (address[] memory candidates) {
         return getAllCandidates(POLICY_ROLE);
     }
 
+    /// @inheritdoc IAppAuthority
     function getAllReserveManagerCandidates() external view returns (address[] memory candidates) {
         return getAllCandidates(RESERVE_MANAGER_ROLE);
     }
 
+    /// @inheritdoc IAppAuthority
     function getAllGuardianCandidates() external view returns (address[] memory candidates) {
         return getAllCandidates(GUARDIAN_ROLE);
     }
 
+    /// @inheritdoc IAppAuthority
     function getAllGovernorCandidates() external view returns (address[] memory candidates) {
         return getAllCandidates(GOVERNOR_ROLE);
     }
 
+    /// @inheritdoc IAppAuthority
     function getAllBondManagerCandidates() external view returns (address[] memory candidates) {
         return getAllCandidates(BOND_MANAGER_ROLE);
     }
--- contracts/interfaces/IAppAuthority.sol
+++ contracts/interfaces/IAppAuthority.sol
@@ -3,43 +3,162 @@ pragma solidity ^0.8.15;
 
 import "./IAppTreasury.sol";
 
+/// @title IAppAuthority
+/// @notice Interface for managing different roles and authorities in the application
+/// @dev This interface defines the core access control functionality for the application
 interface IAppAuthority {
-    /* ========== EVENTS ========== */
-
+    /// @notice Emitted when the treasury address is updated
+    /// @param newTreasury The address of the new treasury
+    /// @param oldTreasury The address of the old treasury
     event TreasuryUpdated(address indexed newTreasury, address indexed oldTreasury);
+
+    /// @notice Emitted when the operations treasury address is updated
+    /// @param newOperationsTreasury The address of the new operations treasury
+    /// @param oldOperationsTreasury The address of the old operations treasury
     event OperationsTreasuryUpdated(address indexed newOperationsTreasury, address indexed oldOperationsTreasury);
 
+    /// @notice Adds a new governor to the system
+    /// @param _newGovernor The address of the new governor to add
     function addGovernor(address _newGovernor) external;
+
+    /// @notice Adds a new guardian to the system
+    /// @param _newGuardian The address of the new guardian to add
     function addGuardian(address _newGuardian) external;
+
+    /// @notice Adds a new policy to the system
+    /// @param _newPolicy The address of the new policy to add
     function addPolicy(address _newPolicy) external;
+
+    /// @notice Adds a new reserve manager to the system
+    /// @param _newReserveManager The address of the new reserve manager to add
     function addReserveManager(address _newReserveManager) external;
+
+    /// @notice Adds a new executor to the system
+    /// @param _newExecutor The address of the new executor to add
     function addExecutor(address _newExecutor) external;
+
+    /// @notice Adds a new bond manager to the system
+    /// @param _newBondManager The address of the new bond manager to add
     function addBondManager(address _newBondManager) external;
 
+    /// @notice Adds a new reserve depositor to the system
+    /// @param _newReserveDepositor The address of the new reserve depositor to add
+    function addReserveDepositor(address _newReserveDepositor) external;
+
+    /// @notice Removes an existing governor from the system
+    /// @param _oldGovernor The address of the governor to remove
     function removeGovernor(address _oldGovernor) external;
+
+    /// @notice Removes an existing guardian from the system
+    /// @param _oldGuardian The address of the guardian to remove
     function removeGuardian(address _oldGuardian) external;
+
+    /// @notice Removes an existing policy from the system
+    /// @param _oldPolicy The address of the policy to remove
     function removePolicy(address _oldPolicy) external;
+
+    /// @notice Removes an existing reserve manager from the system
+    /// @param _oldReserveManager The address of the reserve manager to remove
     function removeReserveManager(address _oldReserveManager) external;
+
+    /// @notice Removes an existing executor from the system
+    /// @param _oldExecutor The address of the executor to remove
     function removeExecutor(address _oldExecutor) external;
+
+    /// @notice Removes an existing bond manager from the system
+    /// @param _oldBondManager The address of the bond manager to remove
     function removeBondManager(address _oldBondManager) external;
 
+    /// @notice Sets the operations treasury address
+    /// @param _newOperationsTreasury The address of the new operations treasury
+    function setOperationsTreasury(address _newOperationsTreasury) external;
+
+    /// @notice Sets the treasury address
+    /// @param _newTreasury The address of the new treasury
+    function setTreasury(address _newTreasury) external;
+
+    /// @notice Removes an existing reserve depositor from the system
+    /// @param _oldReserveDepositor The address of the reserve depositor to remove
+    function removeReserveDepositor(address _oldReserveDepositor) external;
+
+    /// @notice Checks if an address is a governor
+    /// @param account The address to check
+    /// @return bool True if the address is a governor, false otherwise
     function isGovernor(address account) external view returns (bool);
 
+    /// @notice Checks if an address is a bond manager
+    /// @param account The address to check
+    /// @return bool True if the address is a bond manager, false otherwise
     function isBondManager(address account) external view returns (bool);
 
+    /// @notice Checks if an address is a guardian
+    /// @param account The address to check
+    /// @return bool True if the address is a guardian, false otherwise
     function isGuardian(address account) external view returns (bool);
 
+    /// @notice Checks if an address is a policy
+    /// @param account The address to check
+    /// @return bool True if the address is a policy, false otherwise
     function isPolicy(address account) external view returns (bool);
 
+    /// @notice Checks if an address is an executor
+    /// @param account The address to check
+    /// @return bool True if the address is an executor, false otherwise
     function isExecutor(address account) external view returns (bool);
 
+    /// @notice Checks if an address is the treasury
+    /// @param account The address to check
+    /// @return bool True if the address is the treasury, false otherwise
     function isTreasury(address account) external view returns (bool);
 
+    /// @notice Checks if an address is a reserve manager
+    /// @param account The address to check
+    /// @return bool True if the address is a reserve manager, false otherwise
     function isReserveManager(address account) external view returns (bool);
 
+    /// @notice Checks if an address is a reserve depositor
+    /// @param account The address to check
+    /// @return bool True if the address is a reserve depositor, false otherwise
     function isReserveDepositor(address account) external view returns (bool);
 
+    /// @notice Returns the address of the operations treasury
+    /// @return address The address of the operations treasury
     function operationsTreasury() external view returns (address);
 
+    /// @notice Returns the treasury contract instance
+    /// @return IAppTreasury The treasury contract instance
     function treasury() external view returns (IAppTreasury);
+
+    /// @notice Returns an array of all candidates for a given role
+    /// @param role The role to get candidates for
+    /// @return candidates Array of addresses that are candidates for the given role
+    function getAllCandidates(bytes32 role) external view returns (address[] memory candidates);
+
+    /// @notice Returns an array of all reserve depositor candidates
+    /// @return candidates Array of addresses that are reserve depositor candidates
+    function getAllReserveDepositorCandidates() external view returns (address[] memory candidates);
+
+    /// @notice Returns an array of all executor candidates
+    /// @return candidates Array of addresses that are executor candidates
+    function getAllExecutorCandidates() external view returns (address[] memory candidates);
+
+    /// @notice Returns an array of all policy candidates
+    /// @return candidates Array of addresses that are policy candidates
+    function getAllPolicyCandidates() external view returns (address[] memory candidates);
+
+    /// @notice Returns an array of all reserve manager candidates
+    /// @return candidates Array of addresses that are reserve manager candidates
+    function getAllReserveManagerCandidates() external view returns (address[] memory candidates);
+
+    /// @notice Returns an array of all guardian candidates
+    /// @return candidates Array of addresses that are guardian candidates
+    function getAllGuardianCandidates() external view returns (address[] memory candidates);
+
+    /// @notice Returns an array of all governor candidates
+    /// @return candidates Array of addresses that are governor candidates
+    function getAllGovernorCandidates() external view returns (address[] memory candidates);
+
+    /// @notice Returns an array of all bond manager candidates
+    /// @return candidates Array of addresses that are bond manager candidates
+    function getAllBondManagerCandidates() external view returns (address[] memory candidates);
 }
