70fb13b452195d24fb0ab388f87bb7ae87bbe7b7
Dre Cronye <dre@rezerve.money>
2025-07-14 19:06:18 +0000

    Updated contracts

--- contracts/oracles/BalancerStableLPOracle.sol
+++ contracts/oracles/BalancerStableLPOracle.sol
@@ -11,7 +11,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
  * @title BalancerLPOracle
  * @notice An oracle for a Balancer LP token.
  */
-contract BalancerLPOracle is BalancerMath, IOracle {
+contract BalancerStableLPOracle is BalancerMath, IOracle {
     IBalancerVault public vault;
     address public pool;
 
